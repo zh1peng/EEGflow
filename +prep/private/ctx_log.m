49029441e1f8667671f8f2d11c76141c198206ef
zh1peng <frueddy@gmail.com>
2026-01-29 11:25:51 +0800

    rm private

--- +prep/private/ctx_log.m
+++
@@ -1,8 +0,0 @@
-function ctx_log(meta, msg)
-%CTX_LOG Log using meta.logger if provided
-    if isfield(meta, 'logger') && ~isempty(meta.logger)
-        meta.logger(msg);
-    else
-        fprintf('%s\n', msg);
-    end
-end
